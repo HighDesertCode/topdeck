ef1f820718df2b9f08b80bd1fc94130ca5829738
Christian Sanchez <dev@highdesertcode.io>
2026-09-02 08:42:58 -0700

    returning correct type

--- data/secrets_retriever.py
+++ data/secrets_retriever.py
@@ -5,4 +5,8 @@ VAULT_URL = "https://kv-topdeck.vault.azure.net"
 
 def fetch_secret(name: str) -> str:
     client = SecretClient(VAULT_URL, DefaultAzureCredential())
-    return client.get_secret(name).value
+    secret = client.get_secret(name).value
+    if secret is None:
+        raise ValueError(f"secret '{name}' exists but has no value")
+
+    return secret
