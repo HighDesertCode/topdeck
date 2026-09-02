f37d36e6ca306c2ccef9e0493f638717abdb9104
Christian Sanchez <dev@highdesertcode.io>
2026-09-02 09:25:07 -0700

    hatchling packaging + entry point

--- data/tcgdex/series.py
+++ data/tcgdex/series.py
@@ -4,7 +4,7 @@ import time
 import polars as pl
 from pydantic import BaseModel
 
-from secrets_retriever import fetch_secret
+from common.secrets_retriever import fetch_secret
 from tcgdex.client import fetch_json
 
 BASE_URL = "/v2/en/series"
